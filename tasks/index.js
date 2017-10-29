30dbad65f17f3384e3e746d8ef4a204bcdf99b13
Andreas Dzialocha <andreas@marmorkuchen.net>
2017-10-29 20:06:25 +0100

    Copy and minify images

--- tasks/index.js
+++ tasks/index.js
@@ -1,9 +1,11 @@
 const renderHtml = require('./renderHtml')
+const renderImages = require('./renderImages')
 const renderScripts = require('./renderScripts')
 const renderStyles = require('./renderStyles')
 
 module.exports = {
   renderHtml,
+  renderImages,
   renderScripts,
   renderStyles,
 }
